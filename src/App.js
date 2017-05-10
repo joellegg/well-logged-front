4e2d3273197c6082dd8eabb9b767a93de11146cf
joellegg <joellegg@bellsouth.net>
2017-05-10 10:36:34 -0500

    included react-select package, adding select to main page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
 import React, { Component } from 'react';
+import Select from 'react-select';
+import 'react-select/dist/react-select.css';
 import './App.css';
 
 class App extends Component {
+  var options = [
+    { value: 'one', label: 'One' },
+    { value: 'two', label: 'Two' }
+  ];
+
+  function logChange(val) {
+    console.log("Selected: " + val);
+  }
+
   render() {
     return (
       <div className="container">
         <div className="left-container">
           <h1>Well Logged</h1>
-          <h3 className="about-justified">The process is easy. Enter the 10 digit API number of the well your interested in, selected the logs you want, and press download. The logs will automagically download to your system. Eventually the data will be converted to txt files by using machine learning principles</h3>
+          <h3 className="about-justified">The process is simple. Enter the 10 digit API number of the well your interested in, select the logs you want, and download. The logs will automagically download to your system. Eventually the data will be converted to txt files using machine learning principles. For now, enjoy the simplicity.</h3>
           <div className="api-form">
             <label for="api_search">API no.</label>
+            <Select
+              name="form-field-name"
+              value="one"
+              options={options}
+              onChange={logChange}
+            />
             <input id="api-form-input" type="text" name="api_search" placeholder="##-###-#####" />
             <button id="api-form-submit">Download</button>
           </div>
