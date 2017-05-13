829616c5a791d8ddcc3c422e33eaf98e9cba045a
joellegg <joellegg@bellsouth.net>
2017-05-12 20:04:18 -0500

    changed background image, working on displaying async options in react-select menu

--- src/components/githubUsers.js
+++ src/components/githubUsers.js
@@ -4,6 +4,8 @@ import Select from 'react-select';
 import axios from 'axios';
 import './logs.css'
 
+// select distinct on (api) * from api_docs where api like '05-123-2345%' limit 10
+
 const GithubUsers = React.createClass({
   // displayName: 'There will be logs',
   propTypes: {
@@ -11,6 +13,7 @@ const GithubUsers = React.createClass({
   },
   getInitialState () {
     return {
+      options: [],
       backspaceRemoves: true,
       multi: false
     };
@@ -23,11 +26,11 @@ const GithubUsers = React.createClass({
   getLogs (input) {
     if (!input) {
       return Promise.resolve({ options: [] });
-    } else if (input.length > 7) {
+    } else if (input.length > 11) {
       return axios.get(`http://localhost:8080/v1/apis/${input}`)
-        .then(response => response.json)
-        .then((json) => {
-          return { options: json.items };
+        .then((response) => {
+          console.log(response.data)
+          this.setState({ options: response.data })
         })
         .catch(err => console.log(err));
     }
@@ -36,10 +39,7 @@ const GithubUsers = React.createClass({
     window.open(value.html_url)
   },
   render () {
-    const AsyncComponent = this.state.creatable
-      ? Select.AsyncCreatable
-      : Select.Async;
-
+    const AsyncComponent = this.state.creatable ? Select.AsyncCreatable : Select.Async;
     return (
       <div className="section">
         <h3 className="section-heading">{this.props.label}</h3>
