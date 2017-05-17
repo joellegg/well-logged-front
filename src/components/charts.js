a7fc2cadb77b441e3d109fd645da64e81aac4ba3
joellegg <joellegg@bellsouth.net>
2017-05-17 14:20:20 -0500

    switching between count logs by doc and count logs by county with initial count by doc

--- src/components/charts.js
+++ src/components/charts.js
@@ -6,27 +6,39 @@ class Charts extends Component {
     super(props)
     this.state = {
       data: [],
+      chartCount: true,
     }
     this.logCountDoc = this.logCountDoc.bind(this)
+    this.logCountCounty = this.logCountCounty.bind(this)
+  }
+
+  componentDidMount() {
+    return axios.get(`http://localhost:8080/v1/countLogs`)
+      .then((response) => {
+        let data = response.data.rows.map((res) => {
+          return { doc_type:res.doc_type, count: res.count}
+        })
+        console.log('data', data)
+        this.setState({ chartCount: true, data: data })
+      })
+      .catch(err => console.log(err));
   }
 
   logCountDoc() {
     return axios.get(`http://localhost:8080/v1/countLogs`)
       .then((response) => {
         let data = response.data.rows.map((res) => {
-          // if (res.doc_type !== 'n/a') {
-          //   console.log('doc, count', res.doc_type, res.count)
-          // }
-            return { doc_type:res.doc_type, count: res.count}
+          return { doc_type:res.doc_type, count: res.count}
         })
         console.log('data', data)
-        this.setState({ data })
+        this.setState({ chartCount: true, data: data })
       })
       .catch(err => console.log(err));
   }
 
   logCountCounty() {
     console.log('count by county')
+    this.setState({ chartCount: false, data: [] })
   }
 
   render () {
@@ -34,16 +46,16 @@ class Charts extends Component {
       <div>
         <h1>Charts display below</h1>
         <label className="checkbox">
-          <input type="radio" className="checkbox-control" onChange={this.logCountDoc}/>
+          <input type="radio" className="checkbox-control" checked={this.state.chartCount} onChange={this.logCountDoc}/>
           <span className="checkbox-label">log count by doc type</span>
-          <input type="radio" className="checkbox-control" onChange={this.logCountCounty}/>
+          <input type="radio" className="checkbox-control" checked={!this.state.chartCount} onChange={this.logCountCounty}/>
           <span className="checkbox-label">log count by county</span>
         </label>
         <div>
           <h3>Available logs will display below</h3>
           {this.state.data.map(function(log) {
             return (
-              <h3 key={log.doc_type}>{log.doc_type} {log.count}</h3>
+              <p key={log.doc_type}>{log.doc_type} {log.count}</p>
             );
           })}
         </div>
