2ac0a94f0008bd451de2bd9b403b59663b4b98e7
joellegg <joellegg@bellsouth.net>
2017-05-17 11:57:56 -0500

    display log counts as text

--- src/components/charts.js
+++ src/components/charts.js
@@ -1,9 +1,32 @@
-// select doc_type, count(doc_type) from api_docs having count(doc_type) > ? group by doc_type order by count(doc_type) desc;
 import React, {Component} from 'react'
+import axios from 'axios';
 
 class Charts extends Component {
-  logSomething() {
-    console.log('clicked!')
+  constructor (props) {
+    super(props)
+    this.state = {
+      data: [],
+    }
+    this.logCountDoc = this.logCountDoc.bind(this)
+  }
+
+  logCountDoc() {
+    return axios.get(`http://localhost:8080/v1/countLogs`)
+      .then((response) => {
+        let data = response.data.rows.map((res) => {
+          // if (res.doc_type !== 'n/a') {
+          //   console.log('doc, count', res.doc_type, res.count)
+          // }
+            return { doc_type:res.doc_type, count: res.count}
+        })
+        console.log('data', data)
+        this.setState({ data })
+      })
+      .catch(err => console.log(err));
+  }
+
+  logCountCounty() {
+    console.log('count by county')
   }
 
   render () {
@@ -11,9 +34,19 @@ class Charts extends Component {
       <div>
         <h1>Charts display below</h1>
         <label className="checkbox">
-          <input type="radio" className="checkbox-control" onChange={this.logSomething}/>
-          <span className="checkbox-label">Single Value</span>
+          <input type="radio" className="checkbox-control" onChange={this.logCountDoc}/>
+          <span className="checkbox-label">log count by doc type</span>
+          <input type="radio" className="checkbox-control" onChange={this.logCountCounty}/>
+          <span className="checkbox-label">log count by county</span>
         </label>
+        <div>
+          <h3>Available logs will display below</h3>
+          {this.state.data.map(function(log) {
+            return (
+              <h3 key={log.doc_type}>{log.doc_type} {log.count}</h3>
+            );
+          })}
+        </div>
       </div>
     );
   }
